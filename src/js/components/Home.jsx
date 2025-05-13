125fcee2e089a32f7a3ced41fdf2beace7deb415
Dixthor <carlosvillalobos9704@gmail.com>
2025-05-13 14:56:25 +0000

    cree el fetch

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,13 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [tareas, setTareas] = useState([]);
+
+
+	const crearUsuario = () => {
+		fetch('https://playground.4geeks.com/todo/users/CarlosVillalobos', {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+		})
+			.then(response => response.json())
+			.then(data => console.log(data.todos));
+			// .catch((error)=>console.log(error))
+	}
+
+	const traerUsuario = () => {
+		fetch('https://playground.4geeks.com/todo/users/CarlosVillalobos', {
+			method: "GET",
+		})
+			.then(response => response.json())
+			.then(data => console.log(data));
+	}
+
+	useEffect(() => {
+		crearUsuario()
+		traerUsuario()
+	})
+
+
 	return (
 		<div className="text-center">
-            
+
 
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
